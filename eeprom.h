a4cd67eff4226e5246f4e64bc12c2ab67b64b473
Norbert Bielak <norbertbielak@gmail.com>
2020-04-04 14:17:38 +0200

    Fixed sleep mode bugs

--- eeprom.h
+++ eeprom.h
@@ -15,10 +15,11 @@
 void eeprom_update_speed(void);
 void eeprom_update_duration(void);
 void eeprom_save_actual_pos(void);
-void eeprom_save_stop_flag(void);
+void eeprom_update_time_to_sleep(void);
+
 void eeprom_read_speed(void);
 void eeprom_read_duration(void);
 void eeprom_read_actual_pos(void);
-void eeprom_read_stop_flag(void);
+void eeprom_read_time_to_sleep(void);
 
 #endif /* EEPROM_H_ */
